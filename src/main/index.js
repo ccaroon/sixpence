e1d2c509e1cad1f9e2760ca54fb43fcf43cbdb4e
Craig N. Caroon <ccaroon@webassign.net>
2018-02-05 13:55:29 -0500

    - uppercased name in package.json - Added icons - commented out use of "storage" and "axios"; probably don't need those - changed initial window size - if close last window, quit app

--- src/main/index.js
+++ src/main/index.js
@@ -20,9 +20,9 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 928,
     useContentSize: true,
-    width: 1000
+    width: 1280
   })
 
   mainWindow.loadURL(winURL)
@@ -35,9 +35,9 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  // if (process.platform !== 'darwin') {
+  app.quit()
+  // }
 })
 
 app.on('activate', () => {
